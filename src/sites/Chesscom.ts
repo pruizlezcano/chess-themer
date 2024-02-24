bfaf267edc8f1d38f42c79b50902b4f9360f494b
Pablo Ruiz <37554617+pruizlezcano@users.noreply.github.com>
2024-02-24 21:14:09 +0100

    feat: refactor theme change functionality to use abstract class and subclasses for different chess sites

---
+++ src/sites/Chesscom.ts
@@ -0,0 +1,54 @@
+import Site from './Site';
+import { getBoardURL, getPieceURL } from '../utils';
+
+/** Class for chess.com */
+class ChessCom extends Site {
+  constructor() {
+    super();
+  }
+
+  changeBoard(theme: string): void {
+    const board: HTMLElement | null = document.querySelector('wc-chess-board');
+    if (board != null)
+      board.style.backgroundImage = `url('${getBoardURL(theme)}')`;
+    const root: HTMLElement = document.querySelector(':root')!;
+    root.style.setProperty(
+      '--theme-board-style-image',
+      `url('${getBoardURL(theme)}')`,
+    );
+  }
+
+  changePieces(theme: string): void {
+    const pieces: NodeListOf<HTMLElement> = document.querySelectorAll('.piece');
+    const pieceNames = [
+      'wp',
+      'wr',
+      'wn',
+      'wb',
+      'wq',
+      'wk',
+      'bp',
+      'br',
+      'bn',
+      'bb',
+      'bq',
+      'bk',
+    ];
+    const root: HTMLElement = document.querySelector(':root')!;
+    for (const piece of pieceNames) {
+      root.style.setProperty(
+        `--theme-piece-set-${piece}`,
+        `url('${getPieceURL(theme, piece)}')`,
+      );
+    }
+    for (const piece of pieces) {
+      const classList = piece.classList;
+      const name = pieceNames.find((name) => classList.contains(name));
+      if (name != null) {
+        piece.style.backgroundImage = `var(--theme-piece-set-${name})`;
+      }
+    }
+  }
+}
+
+export default ChessCom;
