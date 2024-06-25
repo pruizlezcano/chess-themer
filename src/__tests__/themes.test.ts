4663cddfcf2eee3f3585059c28befe2c832ddbb5
Pablo Ruiz <37554617+pruizlezcano@users.noreply.github.com>
2024-06-25 19:21:36 +0200

    tests working

--- src/__tests__/themes.test.ts
+++ src/__tests__/themes.test.ts
@@ -4,13 +4,6 @@ import fs from 'fs';
 
 const THEMES_PATH = './public/chess-themer';
 
-describe('Print directory', () => {
-  it('Print directory', () => {
-    const tree = fs.readdirSync(THEMES_PATH, { withFileTypes: true });
-    console.log(tree);
-  });
-});
-
 describe('Board themes', () => {
   for (const boardTheme of themes.boards) {
     it(`Board theme '${boardTheme.name}' should exists in folder`, () => {
@@ -22,31 +15,31 @@ describe('Board themes', () => {
   }
 });
 
-// describe('Piece themes', () => {
-//   for (const pieceTheme of themes.pieces) {
-//     describe(`Theme: ${pieceTheme.name}`, () => {
-//       const pieces = [
-//         'bB',
-//         'bK',
-//         'bN',
-//         'bP',
-//         'bQ',
-//         'bR',
-//         'wB',
-//         'wK',
-//         'wN',
-//         'wP',
-//         'wQ',
-//         'wR',
-//       ];
-//       for (const piece of pieces) {
-//         it(`Piece '${piece}' should exist in the folder for theme '${pieceTheme.name}'`, () => {
-//           const file = fs.existsSync(
-//             `${THEMES_PATH}/pieces/${pieceTheme.name}/${piece}.png`,
-//           );
-//           expect(file).toBeTruthy();
-//         });
-//       }
-//     });
-//   }
-// });
+describe('Piece themes', () => {
+  for (const pieceTheme of themes.pieces) {
+    describe(`Theme: ${pieceTheme.name}`, () => {
+      const pieces = [
+        'bB',
+        'bK',
+        'bN',
+        'bP',
+        'bQ',
+        'bR',
+        'wB',
+        'wK',
+        'wN',
+        'wP',
+        'wQ',
+        'wR',
+      ];
+      for (const piece of pieces) {
+        it(`Piece '${piece}' should exist in the folder for theme '${pieceTheme.name}'`, () => {
+          const file = fs.existsSync(
+            `${THEMES_PATH}/pieces/${pieceTheme.name}/${piece}.png`,
+          );
+          expect(file).toBeTruthy();
+        });
+      }
+    });
+  }
+});
