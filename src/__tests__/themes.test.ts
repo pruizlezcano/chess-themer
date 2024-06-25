5ddf8680a47157c336228593c589f5b5401b4633
Pablo Ruiz <37554617+pruizlezcano@users.noreply.github.com>
2024-06-25 19:17:57 +0200

    why are test failing?

--- src/__tests__/themes.test.ts
+++ src/__tests__/themes.test.ts
@@ -2,44 +2,51 @@ import { themes } from '../themes';
 import { describe, expect, it } from 'vitest';
 import fs from 'fs';
 
-const THEMES_PATH = 'public/chess-themer';
+const THEMES_PATH = './';
 
-describe('Board themes', () => {
-  for (const boardTheme of themes.boards) {
-    it(`Board theme '${boardTheme.name}' should exists in folder`, () => {
-      const file = fs.existsSync(
-        `${THEMES_PATH}/boards/${boardTheme.name}/200.png`,
-      );
-      expect(file).toBeTruthy();
-    });
-  }
+describe('Print directory', () => {
+  it('Print directory', () => {
+    const tree = fs.readdirSync(THEMES_PATH, { withFileTypes: true });
+    console.log(tree);
+  });
 });
 
-describe('Piece themes', () => {
-  for (const pieceTheme of themes.pieces) {
-    describe(`Theme: ${pieceTheme.name}`, () => {
-      const pieces = [
-        'bB',
-        'bK',
-        'bN',
-        'bP',
-        'bQ',
-        'bR',
-        'wB',
-        'wK',
-        'wN',
-        'wP',
-        'wQ',
-        'wR',
-      ];
-      for (const piece of pieces) {
-        it(`Piece '${piece}' should exist in the folder for theme '${pieceTheme.name}'`, () => {
-          const file = fs.existsSync(
-            `${THEMES_PATH}/pieces/${pieceTheme.name}/${piece}.png`,
-          );
-          expect(file).toBeTruthy();
-        });
-      }
-    });
-  }
-});
+// describe('Board themes', () => {
+//   for (const boardTheme of themes.boards) {
+//     it(`Board theme '${boardTheme.name}' should exists in folder`, () => {
+//       const file = fs.existsSync(
+//         `${THEMES_PATH}/boards/${boardTheme.name}/200.png`,
+//       );
+//       expect(file).toBeTruthy();
+//     });
+//   }
+// });
+
+// describe('Piece themes', () => {
+//   for (const pieceTheme of themes.pieces) {
+//     describe(`Theme: ${pieceTheme.name}`, () => {
+//       const pieces = [
+//         'bB',
+//         'bK',
+//         'bN',
+//         'bP',
+//         'bQ',
+//         'bR',
+//         'wB',
+//         'wK',
+//         'wN',
+//         'wP',
+//         'wQ',
+//         'wR',
+//       ];
+//       for (const piece of pieces) {
+//         it(`Piece '${piece}' should exist in the folder for theme '${pieceTheme.name}'`, () => {
+//           const file = fs.existsSync(
+//             `${THEMES_PATH}/pieces/${pieceTheme.name}/${piece}.png`,
+//           );
+//           expect(file).toBeTruthy();
+//         });
+//       }
+//     });
+//   }
+// });
